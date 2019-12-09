1a11db2e34498c6ae3dfc17fffb377449d82bde1
Maddy <mmcintosh2k14@gmail.com>
2019-12-09 16:34:18 -0500

    creates GET endpoint to retrieve all accounts

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express');
 
+// database access using knex
 const db = require('./data/dbConfig.js');
 
 const server = express();
 
 server.use(express.json());
+
+// return a list of accounts from db
+server.get('/', (req, res) => {
+        db
+        .select('*')
+        .from('accounts')
+        .then(accounts => {
+            res.status(200)
+            .json(accounts);
+        })
+        .catch(err => {
+            console.log('Problem retrieving accounts.', err);
+            res.status(500).json({ error: 'Error getting accounts.' })
+        });
+});
 
 module.exports = server;
